ad95614fa1b068b44b8eb927a9ba21432981e1ee
Papsjay <donallotei@gmail.com>
2023-01-07 09:21:05 +0000

    model

--- models/review.py
+++ models/review.py
@@ -1,38 +1,23 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
-"""Review module for the HBNB project """
-from models.base_model import BaseModel, Base
-=======
-#!/usr/bin/python
-""" holds class Review"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
->>>>>>> 79385e0c230200caf0a7a21ad750519333f39191
-from sqlalchemy import Column, String, ForeignKey
+"""Defines the Review class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
-<<<<<<< HEAD
-    """ Review class to store review information """
+    """Represents a review for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Reviews.
+        text (sqlalchemy String): The review description.
+        place_id (sqlalchemy String): The review's place id.
+        user_id (sqlalchemy String): The review's user id.
+    """
     __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    text = Column(String(1024), nullable=False)
-=======
-    """Representation of Review """
-    if models.storage_t == 'db':
-        __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
->>>>>>> 79385e0c230200caf0a7a21ad750519333f39191
