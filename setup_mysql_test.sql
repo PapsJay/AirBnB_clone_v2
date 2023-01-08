60e2754142254f381c1d5ff6672e1c666476ccde
Papsjay <donallotei@gmail.com>
2023-01-08 07:34:28 +0000

    setupmysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
--- prepares a MySQL server for the project
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+-- This script prepares a  MySQL server for the project ( Setup test)
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY "hbnb_test_pwd";
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO  hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* TO  hbnb_test@localhost;
